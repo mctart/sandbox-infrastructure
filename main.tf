11678f1ac34a17c54c236ad9c477a677f43b4062
Wayne Evans <evans.wayne@gmail.com>
2023-05-05 00:18:54 +0100

    terraform code update for my app

--- main.tf
+++ main.tf
@@ -1,28 +1,19 @@
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  default_tags {
-    tags = {
-      Environment = terraform.workspace
-      Owner       = "Acme"
-      Provisioned = "Terraform"
-    }
-  }
 }
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
 
-module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "4.0.1"
-  name               = "my-vpc-terraform"
-  cidr               = "10.0.0.0/16"
-  azs                = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
-  #enable_vpn_gateway = true
+
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
   tags = {
-    Name        = "VPC from Module"
+    Name        = var.vpc_name
+    Environment = "demo_environment"
     Terraform   = "true"
-    Environment = "dev"
+    Region      = data.aws_region.current.name
   }
 }
